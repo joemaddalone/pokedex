99285f3f6f0d2f9a1fb651f23b3dbc4f7283fccd
Joe Maddalone <>
2020-07-14 22:37:23 -0500

    Home i18n

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import EmptyState from './common/EmptyState';
+import { translate } from 'poke-i18n';
+
+const t = translate(['home']);
 
 const Home = () => (
   <EmptyState>
-    <h2>Yo dawg, select a type.</h2>
+    <h2>{t('welcome')}</h2>
   </EmptyState>
 );
 
