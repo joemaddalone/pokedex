dd3f07a3ddee901349ee4662a622ee28dce8d0b8
Joe Maddalone <>
2020-07-14 19:46:43 -0500

    css organization and fixed the webpack problem importing font icons

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,6 +3,60 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const appSrc = path.resolve(__dirname, '../src');
+
+// "postcss-flexbugs-fixes": "^4.1.0",
+//     "postcss-import": "^12.0.1",
+//     "postcss-loader": "^3.0.0",
+//     "postcss-preset-env": "^6.6.0",
+
+// common function to get style loaders
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    require.resolve('style-loader'),
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: { publicPath: '../../' },
+    },
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      // Options for PostCSS as we reference these options twice
+      // Adds vendor prefixing based on your specified browser support in
+      // package.json
+      loader: require.resolve('postcss-loader'),
+      options: {
+        // Necessary for external CSS imports to work
+        // https://github.com/facebook/create-react-app/issues/2677
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-preset-env')({
+            stage: 0,
+            nesting: true,
+          }),
+          require('autoprefixer')(),
+          require('cssnano')({
+            autoprefixer: false,
+          }),
+          require('postcss-flexbugs-fixes')(),
+        ],
+        sourceMap: true,
+      },
+    },
+  ].filter(Boolean);
+  if (preProcessor) {
+    loaders.push({
+      loader: require.resolve(preProcessor),
+      options: {
+        sourceMap: true,
+      },
+    });
+  }
+  return loaders;
+};
+
 module.exports = {
   entry: {
     app: './src/index.js',
@@ -11,50 +65,52 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: appSrc,
         use: ['babel-loader', 'eslint-loader'],
       },
+
       {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
+        oneOf: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
             options: {
-              hmr: true,
-              reloadAll: true,
+              limit: 10000,
             },
           },
-          'css-loader',
+          // "postcss" loader applies autoprefixer to our CSS.
+          // "css" loader resolves paths in CSS and adds assets as dependencies.
+          // "style" loader turns CSS into JS modules that inject <style> tags.
+          // In production, we use MiniCSSExtractPlugin to extract that CSS
+          // to a file, but in development "style" loader enables hot editing
+          // of CSS.
+          // By default we support CSS Modules with the extension .module.css
           {
-            loader: 'postcss-loader',
-            options: {
+            test: /\.css$/,
+            use: getStyleLoaders({
+              importLoaders: 1,
               sourceMap: true,
-              plugins: (loader) => [
-                require('postcss-import')({
-                  root: loader.resourcePath,
-                }),
-                require('postcss-preset-env')({
-                  stage: 0,
-                  nesting: true,
-                }),
-                require('autoprefixer')(),
-                require('cssnano')({
-                  autoprefixer: false,
-                }),
-                require('postcss-flexbugs-fixes')(),
-              ],
+            }),
+            sideEffects: true,
+          },
+          // "file" loader makes sure those assets get served by WebpackDevServer.
+          // When you `import` an asset, you get its (virtual) filename.
+          // In production, they would get copied to the `build` folder.
+          // This loader doesn't use a "test" so it will catch all modules
+          // that fall through the other loaders.
+          {
+            loader: require.resolve('file-loader'),
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise be processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              esModule: false,
             },
           },
+          // ** STOP ** Are you adding a new loader?
+          // Make sure to add the new loader(s) before the "file" loader.
         ],
       },
       {
